7b77f98b551ee62a2916f6b2a89af7d1609bab1e
Alex Collin <finapocalypse@gmail.com>
2017-11-26 17:16:13 +0200

    Final commit.

--- public/js/main.js
+++ public/js/main.js
@@ -12,53 +12,53 @@ var api = {
             } catch (error) {} finally {
                 if (typeof callback === "function") callback(result);
             }
-        }).fail(function(result){
+        }).fail(function(result) {
             if (typeof callback === "function") callback(result);
         })
     },
-    getScreen2: function (data, callback) {
+    getScreen2: function(data, callback) {
         this.ajax({
             url: "/screen2",
             method: "POST",
             data: data
         }, callback);
     },
-    getScreen3: function (data, callback) {
+    getScreen3: function(data, callback) {
         this.ajax({
             url: "/screen3",
             method: "POST",
             data: data
         }, callback);
     },
-    getScreen4: function (data, callback) {
+    getScreen4: function(data, callback) {
         this.ajax({
             url: "/screen4",
             method: "POST",
             data: data
         }, callback);
     },
-    getScreen5: function (data, callback) {
+    getScreen5: function(data, callback) {
         this.ajax({
             url: "/screen5",
             method: "POST",
             data: data
         }, callback);
     },
-    getScreen6: function (data, callback) {
+    getScreen6: function(data, callback) {
         this.ajax({
             url: "/screen6",
             method: "POST",
             data: data
         }, callback);
     },
-    getScreen7: function (data, callback) {
+    getScreen7: function(data, callback) {
         this.ajax({
             url: "/screen7",
             method: "POST",
             data: data
         }, callback);
     },
-    getScreen8: function (data, callback) {
+    getScreen8: function(data, callback) {
         this.ajax({
             url: "/screen8",
             method: "POST",
@@ -71,36 +71,36 @@ var app = {
     templates: {
         alert: `<div id="{id}" class="message bg-danger text-light">{message}</div>`
     },
-    alert: function(msg){
+    alert: function(msg) {
         var id = `message-${new Date().getTime() % 100000}${Math.floor(Math.random()*100000)}`;
         $("#message-container").append(this.templates.alert.replace('{id}', id).replace('{message}', msg));
-        setTimeout(function(){
-            $(`#${id}`).fadeOut(400, function(){
+        setTimeout(function() {
+            $(`#${id}`).fadeOut(400, function() {
                 $(this).remove();
             });
         }, 10000);
     },
-    loadScreen2: function(){
+    loadScreen2: function() {
         let user_id = $("#user_id").val();
         if (!user_id) {
             return app.alert("Αυτό πεδίο δεν μπορεί να είναι άδειο");
         }
         api.getScreen2({
             user_id: user_id
-        }, function(data){
+        }, function(data) {
             if (data.success) {
                 $("#wrapper").append(data.html);
                 $("#to-step-2").hide();
                 $("#to-step-2").parents(".input-group").parent().append('<div class="text-center pt-4"><div class="text-light bg-success d-inline p-2">Λάβατε κωδικό επιβεβαίωσης</div></div>')
                 $("#user_id").attr("disabled", "");
                 app.changeCost(costos2f(2017 - parseInt(/(\d{4})/g.exec($("#birth-date").val())), 0, 0, 0));
-                
+
             } else {
                 app.alert(data.description);
             }
         })
     },
-    loadScreen3: function(){
+    loadScreen3: function() {
         let name = $("#name").val();
         let vat = $("#vat").val();
         let amka = $("#amka").val();
@@ -117,7 +117,7 @@ var app = {
             birth_date: birth_date,
             telephone: telephone,
             profession: profession
-        }, function(data){
+        }, function(data) {
             if (data.success) {
                 $("#wrapper").append(data.html);
                 $("#to-step-3").hide();
@@ -132,14 +132,14 @@ var app = {
             }
         })
     },
-    loadScreen4: function(){
+    loadScreen4: function() {
         let countries = [];
-        $(".country:checked").toArray().forEach(function(country){
+        $(".country:checked").toArray().forEach(function(country) {
             countries.push($(country).attr("id"));
         });
         api.getScreen4({
             countries: countries
-        }, function(data){
+        }, function(data) {
             if (data.success) {
                 $("#wrapper").append(data.html);
                 $("#to-step-4").hide();
@@ -150,14 +150,14 @@ var app = {
             }
         })
     },
-    loadScreen5: function(){
+    loadScreen5: function() {
         let hospitals = [];
-        $(".hospital.selected").toArray().forEach(function(hosp){
+        $(".hospital.selected").toArray().forEach(function(hosp) {
             hospitals.push($(hosp).attr("id"));
         });
         api.getScreen5({
             hospitals: hospitals
-        }, function(data){
+        }, function(data) {
             if (data.success) {
                 $("#wrapper").append(data.html);
                 $("#to-step-5").hide();
@@ -167,13 +167,13 @@ var app = {
             }
         })
     },
-    loadScreen6: function(){
+    loadScreen6: function() {
         let coverage = {};
         coverage.min = parseInt($("#selector").slider("values", 0));
         coverage.max = parseInt($("#selector").slider("values", 1));
         api.getScreen6({
             coverage: coverage
-        }, function(data){
+        }, function(data) {
             if (data.success) {
                 $("#wrapper").append(data.html);
                 $("#to-step-6").hide();
@@ -182,11 +182,11 @@ var app = {
             }
         })
     },
-    loadScreen7: function(){
+    loadScreen7: function() {
         let contract_type = $(".contract-type.btn-primary").html("ΕΤΗΣΙΟ") ? true : false;
         api.getScreen7({
             contract_type: contract_type
-        }, function(data){
+        }, function(data) {
             if (data.success) {
                 $("#wrapper").append(data.html);
                 $("#to-step-7").hide();
@@ -195,11 +195,11 @@ var app = {
             }
         })
     },
-    loadScreen8: function(){
+    loadScreen8: function() {
         // let contract_type = $(".contract-type.btn-primary").html("ΕΤΗΣΙΟ") ? true : false;
         api.getScreen8({
             // contract_type: contract_type
-        }, function(data){
+        }, function(data) {
             if (data.success) {
                 $("#wrapper").append(data.html);
                 $("#to-step-8").hide();
@@ -223,37 +223,34 @@ var app = {
         $("#amount").html("€" + $("#selector").slider("values", 0) + " - €" + $("#selector").slider("values", 1));
     },
     erwtiseisKapnismatos: function(a) {
-        
-        if (a =='y'){
+
+        if (a == 'y') {
             $('#erwtKapn1 , #erwtKapn2').removeClass('hidden')
-        }
-        else if(a == 'n'){
+        } else if (a == 'n') {
             $('#erwtKapn1 , #erwtKapn2').addClass('hidden')
         }
     },
-    
-    erwtiseisKarkinou: function(b){
-        if (b == 'y'){
-            $('#erwtKark1 , #erwtKark2, #erwtKark3' ).removeClass('hidden')
-        }
-        else if(b == 'n'){
-            $('#erwtKark1 , #erwtKark2, #erwtKark3' ).addClass('hidden')
-        }
-    },
-    
-    erwtiseisDiaviti: function(c){
-        if (c == 'y'){
-            $('#erwtDiav1 , #erwtDiav2, #erwtDiav3' ).removeClass('hidden')
-        }
-        else if(c == 'n'){
-            $('#erwtDiav1 , #erwtDiav2, #erwtDiav3' ).addClass('hidden')
+
+    erwtiseisKarkinou: function(b) {
+        if (b == 'y') {
+            $('#erwtKark1 , #erwtKark2, #erwtKark3').removeClass('hidden')
+        } else if (b == 'n') {
+            $('#erwtKark1 , #erwtKark2, #erwtKark3').addClass('hidden')
         }
     },
-    
-    changeCost: function(val){
+
+    erwtiseisDiaviti: function(c) {
+        if (c == 'y') {
+            $('#erwtDiav1 , #erwtDiav2, #erwtDiav3').removeClass('hidden')
+        } else if (c == 'n') {
+            $('#erwtDiav1 , #erwtDiav2, #erwtDiav3').addClass('hidden')
+        }
+    },
+
+    changeCost: function(val) {
         $("#cost-number").html(val);
     },
-    events: function(){
+    events: function() {
         $("body").on("click", "#to-step-2", app.loadScreen2);
         $("body").on("click", "#to-step-3", app.loadScreen3);
         $("body").on("click", "#to-step-4", app.loadScreen4);
@@ -261,9 +258,9 @@ var app = {
         $("body").on("click", "#to-step-6", app.loadScreen6);
         $("body").on("click", "#to-step-7", app.loadScreen7);
         $("body").on("click", "#to-step-8", app.loadScreen8);
-        
+
         // If font awesome icon is clicked toggle the icon appropriately
-        $("body").on("click", ".toggle-content", function(){
+        $("body").on("click", ".toggle-content", function() {
             app.toggleContent(this);
             if ($(this).parents(".continent").find(".country-list").is(":visible")) {
                 app.toggleIcon(this, false);
@@ -273,7 +270,7 @@ var app = {
         });
 
         // If the continent checkbox is ticked, open the country list div and check all
-        $("body").on("change", ".check-all-countries", function(){
+        $("body").on("change", ".check-all-countries", function() {
             app.toggleContent(this, true);
             app.toggleIcon($(this).parents(".continent"), false);
             $(this).parents(".continent").find(".country-list input[type=checkbox]").prop('checked', $(this).prop('checked'));
@@ -282,7 +279,7 @@ var app = {
         // On country checkbox tick check whether all countries of the checkbox are ticked
         // if yes, then check the continent checkbox
         // otherwise uncheck it
-        $("body").on("change", ".country", function(){
+        $("body").on("change", ".country", function() {
             let parent = $(this).parents(".continent");
             if (parent.find(".country-list input[type=checkbox]").length === parent.find(".country-list input[type=checkbox]:checked").length) {
                 parent.find(".check-all-countries").prop("checked", true);
@@ -290,18 +287,18 @@ var app = {
                 parent.find(".check-all-countries").prop("checked", false);
             }
         });
-        
+
         // Select hospital on click
-        $("body").on("click", ".hospital", function(){
+        $("body").on("click", ".hospital", function() {
             $(this).toggleClass("selected");
         });
         // If select all button is clicked, select all hospitals
-        $("body").on("click", "#select-all-hospitals", function(){
+        $("body").on("click", "#select-all-hospitals", function() {
             $(".hospital").addClass("selected");
         });
-        
-        
-        $("body").on("click", ".contract-type", function(){
+
+
+        $("body").on("click", ".contract-type", function() {
             $(".contract-type").removeClass("btn-primary").addClass("btn-outline-primary");
             $(this).addClass("btn-primary").removeClass("btn-outline-primary");
             app.changeCost(costos2f(
@@ -313,7 +310,7 @@ var app = {
         });
     },
     // Function to toggle display of country list div
-    toggleContent: function (elem, forced) {
+    toggleContent: function(elem, forced) {
         if (forced === true || !$(elem).parents(".continent").find(".country-list").is(":visible")) {
             $(elem).parents(".continent").find(".country-list").removeClass("hidden");
         } else {
@@ -361,7 +358,7 @@ function costos2f(age, posoApalagis, etisiaKalipsi, orioApalagis) {
 }
 
 
-$(document).ready(function(){
+$(document).ready(function() {
     app.events();
-    app.changeCost(costos2f(0, 0, 0, 0));
+    app.changeCost('--');
 });
